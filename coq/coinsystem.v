8e1d028257c67fa11b125d1ab656a6e9f97e16d9
Nadeem Abdul Hamid <nadeem@acm.org>
2013-02-03 21:21:14 -0500

    first implementation of Pearson counterexample algorithm in coq

--- coq/coinsystem.v
+++ coq/coinsystem.v
@@ -23,32 +23,10 @@ Definition coinlist := list nat.
 Definition repr := list nat.
 
 Definition N := 4.
-Definition C : coinlist := 25 :: 10 :: 5 :: 1 :: nil.
+Definition C : coinlist := 25 :: 6 :: 5 :: 1 :: nil.
 
-Lemma ClenN : length C = N.
- auto. Qed.
-Lemma Cdecr : true = decreasing_order C.
-  compute; repeat split; auto; apply leb_complete; auto.
-  Qed.
+Eval compute in decreasing_order C.
 
-(*
- Definition coinlist_valid (C : coinlist)
-    := (length C) = N /\ decreasing_order C.
-
-  Definition repr_valid (R : repr)
-    := (length R) = N.
-  
-  Definition valid_coinlist := { C : coinlist | coinlist_valid C }.
-  Definition valid_repr := { R : repr | repr_valid R }.
-
-  Definition C : valid_coinlist.
-    exists Cbase.
-    repeat split.
-    apply ClengthN.
-    apply Cdecr.
-  Defined.
-*)
-  
 Fixpoint repr_value (V : repr) (C : coinlist) : nat :=  (* inner product V . C *)
   match V, C with
       | nil, nil => 0
@@ -74,6 +52,8 @@ Fixpoint repr_lt (U V : repr) : bool :=
     | _, _ => false
   end.
 
+Definition repr_gt (U V : repr) : bool := repr_lt V U.
+
 Eval compute in (repr_lt (1 :: 1 :: 0 ::  3 :: nil) (1 :: 3 :: 0 :: 0 :: nil)).
 Eval compute in negb (repr_lt (1 :: 1 :: nil) (1 :: 1 :: nil)).
 Eval compute in negb (repr_lt (3 :: 1 :: nil) (1 :: 1 :: nil)).
@@ -96,11 +76,18 @@ Eval compute in 11 mod 3.
 
 Fixpoint range (n:nat) : list nat :=
   match n with
-    | 0 => 0 :: nil
-    | S n' => n :: range n'
+    | 0 => nil
+    | S n' => n' :: range n'
+  end.
+
+Fixpoint range_from (start num : nat) : list nat :=
+  match num with
+    | 0 => nil
+    | S m => start :: range_from (S start) m
   end.
 
 Eval compute in (range 5).
+Eval compute in (range_from 10 5).
 
 Fixpoint cons_each (x:nat) (V:list repr) :=
   match V with
@@ -113,7 +100,7 @@ Fixpoint all_reprs (C : coinlist) (v : nat) {struct C} : list repr :=
       | nil => nil
       | c :: nil => (v :: nil) :: nil
       | c :: C' => let max_of_c := v / c in
-                   let count_of_c_opts := range max_of_c in
+                   let count_of_c_opts := range (S max_of_c) in
                    (fix all_reprs_iterate (X : list nat) : list repr :=
                       match X with
                         | nil => nil
@@ -127,14 +114,103 @@ Fixpoint all_reprs (C : coinlist) (v : nat) {struct C} : list repr :=
 
 Eval compute in (all_reprs C 17).
 
+Definition more_minimal (new : repr) (cur : repr) : bool :=
+  orb (ltb (repr_size new) (repr_size cur))
+      (andb (beq_nat (repr_size new) (repr_size cur))
+            (repr_lt cur new)).
+
+Fixpoint make_repr_all_ones (n:nat) (v:nat) : repr :=
+  match n with
+    | 0 => nil
+    | 1 => v :: nil
+    | S n' => 0 :: make_repr_all_ones n' v
+  end.
+
+(* brute force computations of the minimal and greedy representations *)
+Definition minimal_bf v := best_of more_minimal (make_repr_all_ones N v) (all_reprs C v).
+Definition greedy_bf v :=  best_of repr_gt  (make_repr_all_ones N v) (all_reprs C v).
+
+Fixpoint greedy_gen (C:coinlist) (v:nat) : repr :=
+  match C with
+    | nil => nil
+    | c :: C' => let q := v / c in
+                 let r := v mod c in
+                 q :: greedy_gen C' r
+  end.
+Definition greedy := greedy_gen C.
+  
+Eval compute in (make_repr_all_ones 4 17).
+Eval compute in (more_minimal  (0 :: 1 :: 1 :: 2 :: nil) (0 :: 0 :: 1 :: 12 :: nil)).
+Eval compute in (more_minimal  (1 :: 2 :: 1 :: 0 :: nil) (0 :: 1 :: 1 :: 2 :: nil) ).
+
+Eval compute in 
+    let v := 83 in
+    (minimal_bf v , 
+     greedy_bf v, 
+     greedy v).
 
 
+(* Pearson's algorithm to find smallest counterexample *)
+
+Definition targetCvals (C:coinlist) : coinlist :=
+  map (fun c => c - 1) C.
+
+Eval compute in targetCvals (25 :: 10 :: 5 :: 1 :: nil).
+
+Definition greedy_multi (C:coinlist) (V : list nat) : list repr :=
+  map (greedy_gen C) V.
+
+Eval compute in (greedy_multi C (targetCvals C)).
+
+Definition zero_out : repr -> repr :=
+  map (fun x => 0).
+
+Fixpoint generate_possible_ce (R : repr) (i : nat) : repr :=
+  match R, i with
+    | x :: R', 0 => x+1 :: zero_out R'
+    | x :: R', S i' => x :: generate_possible_ce R' i'
+    | _, _ => R
+  end.
+
+Definition generate_possible_ces_from (R : repr) (i j : nat) : list repr :=
+  map (generate_possible_ce R) (range_from i (j - i)).
+
+Eval compute in generate_possible_ce (2 :: 1 :: 3 :: 1 :: 2 :: 7 :: nil) 3.
+Eval compute in generate_possible_ces_from (2 :: 1 :: 3 :: 1 :: 2 :: 7 :: nil) 1 4.
+
+Fixpoint app_all (Rs : list (list repr)) : list repr :=
+  match Rs with
+    | nil => nil
+    | x :: Rs' => app x (app_all Rs')
+  end.
+
+Definition generate_min_reprs_to_check (Gs : list repr) : list repr :=
+  app_all (map (fun G => generate_possible_ces_from G 1 N) Gs).
+
+Eval compute in generate_min_reprs_to_check  (greedy_multi C (targetCvals C)).
+
+Definition is_min_lt_greedy_repr (R : repr) : bool :=
+  ltb (repr_size R) (repr_size (greedy (repr_value R C))).
 
 
+Fixpoint findp (A:Type) (f: A -> bool) (As : list A) : option A :=
+  match As with
+    | nil => None
+    | a :: As' => if (f a) then Some a else findp A f As'
+  end.
 
+Definition find_counterexample (C:coinlist) : option nat :=
+  match 
+    findp _ is_min_lt_greedy_repr (generate_min_reprs_to_check  (greedy_multi C (targetCvals C)))
+  with
+    | None => None
+    | Some R => Some (repr_value C R)
+  end.
 
+Eval compute in (find_counterexample C).
 
-
-
-
-
+Eval compute in 
+    let v := 10 in
+    (minimal_bf v , 
+     greedy_bf v, 
+     greedy v).
