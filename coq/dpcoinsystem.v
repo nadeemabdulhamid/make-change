755070cc6cf04bf346cb6ff2e8ae4b4251a872d2
Brook Bowers <brook.bowers@vikings.berry.edu>
2013-02-25 13:04:22 -0500

    Refined dpcoinsystem, need to reformat if then as Ackermann function

--- coq/dpcoinsystem.v
+++ coq/dpcoinsystem.v
@@ -1,27 +1,25 @@
 
 Require Import List.
-Require Import NPeano.
-Require Import EqNat.
-Require Import Compare_dec.
+Require Import NPeano EqNat Compare_dec.
 
 
 Definition min (m : nat) (n : nat) : nat :=
-  if (geb m n) then n
-  else m.
+  if (ltb m n) then m 
+  else n.
 
 
 Fixpoint min_num_coins (n : nat) (vals : list nat) : nat :=
   match n with
-      | 0 => 0
-      | n' => let min_of_options := 
-                  (fix make_change_choices (n : nat) (vals : list nat) : nat :=
-                    match vals with
-                        | nil => 1000
-                        | val :: vals' =>
-                            if  (geb n val) then (min (min_num_coins (n - val) vals)
-                                                     (make_change_choices n vals'))
-                            else (make_change_choices n vals')
-                    end)
-                    in
-                      (S min_of_options)
+    | 0 => 0
+    | n' => let min_of_options :=
+                (fix make_change_choices (n : nat) (vals : list nat) : nat :=
+                 match vals with
+                   | nil => 1000
+                   | val :: vals' => 
+                       if  (ltb val n) then (make_change_choices n vals')
+                       else (min (min_num_coins (n - val) vals)
+                                 (make_change_choices n vals'))
+                 end) n' vals
+            in
+              (S min_of_options)
   end.
