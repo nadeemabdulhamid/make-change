c9bfea1cf3d5c0b541c1ab3bdfcbcf37670d523a
Brook Bowers <brook.bowers@vikings.berry.edu>
2013-02-26 12:25:24 -0500

    Scrapped current version of min_num_coins, got helper function min to work

--- coq/dpcoinsystem.v
+++ coq/dpcoinsystem.v
@@ -3,40 +3,43 @@ Require Import List.
 Require Import NPeano EqNat Compare_dec.
 
 
-Definition min (m : nat) (n : nat) : nat :=
-  if (ltb m n) then m 
-  else n.
+Fixpoint min (n : nat) (m : nat) : nat :=
+  match n with
+      | 0 => 0
+      | S n' => match m with
+                    | 0 => 0
+                    | S m' => S (min n' m')
+                end
+  end.
+
+Eval compute in (min 8 4).
 
 
+
+
+
+(*
 Fixpoint min_num_coins (n : nat) (vals : list nat) : nat :=
   match n with
     | 0 => 0
-    | n' => let fix make_change_choices (n0 : nat) (vals : list nat) : nat :=
-                 match vals with
-                   | nil => 1000
-                   | val :: vals' => let fix try_choice (n1 : nat) (val0: nat) : nat :=
-                                         match n1 with 
-                                             | 0 => 1000
-                                             | S n2 => match val0 with
-                                                           | 0 => (min_num_coins (n0 - val) vals)
-                                                           | S val1 => (try_choice n2 val1)
-                                                       end
-                                             
-                                         end in let fix min (i : nat) (j : nat) : nat :=
-                                                    match i with
-                                                        | 0 => i
-                                                        | S i0 => match j with
-                                                                      | 0 => j
-                                                                      | S j0 => (min i0 j0)
-                                                                  end
-                                                    end
-                                                in (min (try_choice n0 val)
-                                                                (make_change_choices n vals'))
-                                         end
-                                     in
-                                       (S (make_change_choices n' vals))
-                 end.
-
+    | S n' => let fix make_change_choices (vals0 : list nat) (n0 : nat) : nat :=
+                  match vals0 with
+                    | nil => 1000
+                    | val :: vals' => let fix try_choice (n1 : nat) (val0: nat) : nat :=
+                                          match n1 with 
+                                            | 0 => 1000
+                                            | S n2 => match val0 with
+                                                        | 0 => (min_num_coins (n0 - val) vals)
+                                                        | S val1 => (try_choice n2 val1)
+                                                      end
+                                          end
+                                      in (min (try_choice n0 val)
+                                              (make_change_choices vals' n))
+                  end
+              in
+                (S (make_change_choices vals n'))
+  end.
+*)
 
 
 (*Fixpoint min_num_coins (n : nat) (vals : list nat) : nat :=
